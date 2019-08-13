fd7096d987b392925fc9c71d4a7f51559a6713cd
Davor Budimir <47507294+BudimirDavor@users.noreply.github.com>
2019-08-13 19:47:15 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+function password_gen($length = 8) 
+{
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    $password = substr(str_shuffle($chars), 0, $length);
+    return $password;
+}
+echo password_gen();
+?>
